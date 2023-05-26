d29fa3c6dfffbecd777ce3f42834f73349cc011e
JianKang34 <95484251+JianKang34@users.noreply.github.com>
2023-05-26 18:24:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -55,7 +55,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'nWUcrRhKoyK4d6egfiIYEKRDSF8RDt48m0RixaaHqIs	',
+  CALLBACK_TEMPLATE_ID: 'nWUcrRhKoyK4d6egfiIYEKRDSF8RDt48m0RixaaHqIs',
 
   CALLBACK_USERS: [
     {

179390e8e4caadb58b245d0614a26a1b4fb60ffc
JianKang34 <95484251+JianKang34@users.noreply.github.com>
2023-05-26 17:51:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -45,7 +45,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-05-26' },
+        { keyword: 'love_day', date: '2023-05-24' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
